d9e6fdb1b231066f2db6ad34e05372688d5df5ac
Mike Chanslor <mdchansl@southernco.com>
2017-12-28 10:21:21 -0600

    Changed back to wget. updateinfo.xml.gz is the magic

--- ol7-repo-sync.bash
+++ ol7-repo-sync.bash
@@ -1,22 +1,28 @@
 #!/bin/bash
 
 # Mon Aug 28 10:15:44 CDT 2017
-# Thu Dec 28 09:38:15 CST 2017 - Added --download-metadata --downloadcomps
 
 #exec 1> /var/log/ol-repo-sync.log 2>&1
 
-/usr/bin/reposync --download-metadata --downloadcomps -l -m -d -r ol7_latest -p /repo/OL/  > /var/log/ol-repo-sync.ol7_latest.log 2>&1
-cd /repo/OL/ol7_latest
-createrepo -v /repo/OL/ol7_latest/ -g comps.xml >> /var/log/ol-repo-sync.ol7_latest.createrepo.log 2>&1
+#I don't think we need --download-metadata --downloadcomps because we are wgetting
+#/usr/bin/reposync --download-metadata --downloadcomps -l -m -d -r ol7_latest -p /repo/OL/  > /var/log/ol-repo-sync.ol7_latest.log 2>&1
+/usr/bin/reposync -l -m -d -r ol7_latest -p /repo/OL/  > /var/log/ol-repo-sync.ol7_latest.log 2>&1
+
+#I dont think we need to createrepo command since we are wgetting them...
+#cd /repo/OL/ol7_latest
+#createrepo -v /repo/OL/ol7_latest/ -g comps.xml >> /var/log/ol-repo-sync.ol7_latest.createrepo.log 2>&1
 
 #This was obsoleted by --download-metadata --downloadcomps
-#cd /repo/OL/ol7_latest/repodata/
-#/usr/bin/wget -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/comps.xml
-#/usr/bin/wget -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/filelists.xml.gz
-#/usr/bin/wget -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/other.xml.gz
-#/usr/bin/wget -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/primary.xml.gz
-#/usr/bin/wget -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/repomd.xml
-#/usr/bin/wget -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/updateinfo.xml.gz
+cd /repo/OL/ol7_latest/
+rm -rf repodata
+mkdir repodata
+cd /repo/OL/ol7_latest/repodata/
+/usr/bin/wget -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/comps.xml
+/usr/bin/wget -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/filelists.xml.gz
+/usr/bin/wget -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/other.xml.gz
+/usr/bin/wget -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/primary.xml.gz
+/usr/bin/wget -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/repomd.xml
+/usr/bin/wget -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/updateinfo.xml.gz
 
 
 /usr/bin/reposync --download-metadata --downloadcomps -l -m -d -r ol7_UEKR4 -p /repo/OL/  > /var/log/ol-repo-sync.UEKR4.log 2>&1
@@ -30,6 +36,5 @@ createrepo -v /repo/OL/ol7_UEKR4/ -g comps.xml >> /var/log/ol-repo-sync.UEKR4.cr
 #/usr/bin/wget -N http://yum.oracle.com/repo/OracleLinux/OL7/UEKR4/x86_64/repodata/primary.xml.gz
 #/usr/bin/wget -N http://yum.oracle.com/repo/OracleLinux/OL7/UEKR4/x86_64/repodata/repomd.xml
 #/usr/bin/wget -N http://yum.oracle.com/repo/OracleLinux/OL7/UEKR4/x86_64/repodata/updateinfo.xml.gz
-
 
 
