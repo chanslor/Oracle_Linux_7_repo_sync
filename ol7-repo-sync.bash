f3077ac540e3f431e0178aa944a12d92aab4f055
Mike Chanslor <mdchansl@southernco.com>
2018-01-08 12:27:10 -0600

    using wget -q flag. Logs files way to verbose on large down loads.

--- ol7-repo-sync.bash
+++ ol7-repo-sync.bash
@@ -17,12 +17,12 @@ cd /repo/OL/ol7_latest/
 rm -rf repodata
 mkdir repodata
 cd /repo/OL/ol7_latest/repodata/
-/usr/bin/wget -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/comps.xml
-/usr/bin/wget -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/filelists.xml.gz
-/usr/bin/wget -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/other.xml.gz
-/usr/bin/wget -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/primary.xml.gz
-/usr/bin/wget -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/repomd.xml
-/usr/bin/wget -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/updateinfo.xml.gz
+/usr/bin/wget -q -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/comps.xml
+/usr/bin/wget -q -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/filelists.xml.gz
+/usr/bin/wget -q -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/other.xml.gz
+/usr/bin/wget -q -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/primary.xml.gz
+/usr/bin/wget -q -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/repomd.xml
+/usr/bin/wget -q -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/updateinfo.xml.gz
 
 
 /usr/bin/reposync --download-metadata --downloadcomps -l -m -d -r ol7_UEKR4 -p /repo/OL/  > /var/log/ol-repo-sync.UEKR4.log 2>&1
