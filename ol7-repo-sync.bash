588a59f1b8be323fcc43483aba30a55b8cd0d871
Mike Chanslor <mdchansl@southernco.com>
2018-02-15 14:18:52 -0600

    Changes not showing up unless createrepo is run

--- ol7-repo-sync.bash
+++ ol7-repo-sync.bash
@@ -2,9 +2,9 @@
 
 # Mon Aug 28 10:15:44 CDT 2017
 
-#exec 1> /var/log/ol-repo-sync.log 2>&1
+exec 1> /var/log/ol-repo-sync.log 2>&1
 
-/usr/bin/reposync -l -m -d -r ol7_latest -p /repo/OL/  > /var/log/ol-repo-sync.ol7_latest.log 2>&1
+/usr/bin/reposync -l -m -d -r ol7_latest -p /repo/OL/
 
 cd /repo/OL/ol7_latest/
 rm -rf repodata
@@ -17,14 +17,13 @@ cd /repo/OL/ol7_latest/repodata/
 /usr/bin/wget -q -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/repomd.xml
 /usr/bin/wget -q -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/updateinfo.xml.gz
 
+cd /repo/OL/ol7_latest/
+createrepo /repo/OL/ol7_latest/
 
 #Next, the UEK kernel
-
-/usr/bin/reposync --download-metadata --downloadcomps -l -m -d -r ol7_UEKR4 -p /repo/OL/  > /var/log/ol-repo-sync.UEKR4.log 2>&1
 cd /repo/OL/ol7_UEKR4
-createrepo -v /repo/OL/ol7_UEKR4/ -g comps.xml >> /var/log/ol-repo-sync.UEKR4.createrepo.log 2>&1
+/usr/bin/reposync --download-metadata --downloadcomps -l -m -d -r ol7_UEKR4 -p /repo/OL/
 
-#
 #This was obsoleted by --download-metadata --downloadcomps
 cd /repo/OL/ol7_UEKR4/repodata/
 /usr/bin/wget -q -N http://yum.oracle.com/repo/OracleLinux/OL7/UEKR4/x86_64/repodata/filelists.xml.gz
@@ -33,4 +32,6 @@ cd /repo/OL/ol7_UEKR4/repodata/
 /usr/bin/wget -q -N http://yum.oracle.com/repo/OracleLinux/OL7/UEKR4/x86_64/repodata/repomd.xml
 /usr/bin/wget -q -N http://yum.oracle.com/repo/OracleLinux/OL7/UEKR4/x86_64/repodata/updateinfo.xml.gz
 
+cd /repo/OL/ol7_UEKR4
+createrepo /repo/OL/ol7_UEKR4/
 
