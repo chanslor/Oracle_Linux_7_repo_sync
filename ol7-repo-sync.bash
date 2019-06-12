472e99766b0fb2cd5e6ad322768e5cd304654c8c
Mike Chanslor <mdchansl@southernco.com>
2019-06-12 13:35:37 -0500

    Oracle change repo meta data in May 2019

--- ol7-repo-sync.bash
+++ ol7-repo-sync.bash
@@ -4,27 +4,31 @@
 # Wed May  9 11:12:51 CDT 2018 mdchansl@southernco.com
 # createrepo clobbers the CVE data, only do wget
 
+# Wed Jun 12 13:33:41 CDT 2019 - Oracle changed the meta data in MAY 2019
+
 exec 1> /var/log/ol-repo-sync.log 2>&1
 
-/usr/bin/reposync -l -m -d -r ol7_latest -p /repo/OL/
+$REPOSYNC --download-metadata --downloadcomps -l -m -d -r ol7_latest -p /repo/OL/
+$CREATEREPO --update -v /repo/OL/ol7_latest -g /repo/OL/ol7_latest/comps.xml
 
-cd /repo/OL/ol7_latest/
-cd /repo/OL/ol7_latest/repodata/
-/usr/bin/wget -q -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/comps.xml
-/usr/bin/wget -q -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/filelists.xml.gz
-/usr/bin/wget -q -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/other.xml.gz
-/usr/bin/wget -q -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/primary.xml.gz
-/usr/bin/wget -q -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/repomd.xml
-/usr/bin/wget -q -N http://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/repodata/updateinfo.xml.gz
+if [ -d /repo/OL/ol7_latest ] ; then
+        cd /repo/OL/ol7_latest/
+        gzip -d *-updateinfo.xml.gz
+        mv *-updateinfo.xml updateinfo.xml
+        modifyrepo /repo/OL/ol7_latest/updateinfo.xml /repo/OL/ol7_latest/repodata
+fi
+
+
 
 #Next, the UEK kernel
-cd /repo/OL/ol7_UEKR4
-/usr/bin/reposync --download-metadata --downloadcomps -l -m -d -r ol7_UEKR4 -p /repo/OL/
 
-cd /repo/OL/ol7_UEKR4/repodata/
-/usr/bin/wget -q -N http://yum.oracle.com/repo/OracleLinux/OL7/UEKR4/x86_64/repodata/filelists.xml.gz
-/usr/bin/wget -q -N http://yum.oracle.com/repo/OracleLinux/OL7/UEKR4/x86_64/repodata/other.xml.gz
-/usr/bin/wget -q -N http://yum.oracle.com/repo/OracleLinux/OL7/UEKR4/x86_64/repodata/primary.xml.gz
-/usr/bin/wget -q -N http://yum.oracle.com/repo/OracleLinux/OL7/UEKR4/x86_64/repodata/repomd.xml
-/usr/bin/wget -q -N http://yum.oracle.com/repo/OracleLinux/OL7/UEKR4/x86_64/repodata/updateinfo.xml.gz
+$REPOSYNC --download-metadata --downloadcomps -l -m -d -r ol7_UEKR4 -p /repo/OL/
+$CREATEREPO --update -v /repo/OL/ol7_UEKR4
+
+if [ -d /repo/OL/ol7_UEKR4 ] ; then
+        cd /repo/OL/ol7_UEKR4
+        gzip -d *-updateinfo.xml.gz
+        mv *-updateinfo.xml updateinfo.xml
+        modifyrepo /repo/OL/ol7_UEKR4/updateinfo.xml /repo/OL/ol7_UEKR4/repodata
+fi
 
